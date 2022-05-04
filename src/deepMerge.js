e6820aea3d45b4d3aba21481ea09d4f94cb2b89b
Cross <662242@mail.ru>
2022-05-04 22:54:29 +0500

    refakt

--- src/deepMerge.js
+++ src/deepMerge.js
@@ -2,36 +2,31 @@
 const { isArray, isObject, isSamtype } = require('./helpers');
 
 module.exports = deepMerge;
-
 function deepMerge(obj1, obj2) {
 	if (!isObject(obj1) && !isArray(obj1) || !isSamtype(obj1, obj2)) {
-		if (isArray(obj2)) {
-			return deepCopyArray(obj2);
-		}
-		if (isObject(obj2)) {
-			return deepCopyObject(obj2);
+		if (isArray(obj2) || isObject(obj2)) {
+			return deepCopy(obj2);
 		}
 		return obj2;
 	}
-	if (isArray(obj1)) {
-		return deepMergeArray(obj1, obj2);
-	}
-	if (isObject(obj1)) {
+	if (isArray(obj1) || isObject(obj1)) {
 		return deepMergeObject(obj1, obj2);
 	}
 }
-
 //************************************************************ */
-function deepMergeObject(obj1, obj2) {
-	const result = deepCopyObject(obj1);
+function deepMergeObject(obj1, obj2) { //* глубокое копирование
+	if (isArray(obj1) && isArray(obj2)) {
+		return deepCopy([...obj1, ...obj2]);
+	}
+	const result = deepCopy(obj1);
 	for (const key of Object.keys(obj2)) {
 		if (!Object.prototype.hasOwnProperty.call(result, key)) { //*!result.hasOwnProperty(key)
 			if (isArray(obj2[key])) {
-				result[key] = deepCopyArray(obj2[key]);
+				result[key] = deepCopy(obj2[key]);
 				continue;
 			}
 			if (isObject(obj2[key])) {
-				result[key] = deepCopyObject(obj2[key]);
+				result[key] = deepCopy(obj2[key]);
 				continue;
 			}
 			result[key] = obj2[key];
@@ -41,37 +36,15 @@ function deepMergeObject(obj1, obj2) {
 	}
 	return result;
 }
-
-function deepMergeArray(arr1, arr2) {
-	return deepCopyArray([...arr1, ...arr2]);
-}
-
-function deepCopyArray(obj) {
-	const  result = [...obj];
-	for (let index = 0; index < result.length; index++) {
-		if (isArray(result[index])) {
-			result[index] = deepCopyArray(result[index]);
-			continue;
-		}
-		if (isObject(result[index])) {
-			result[index] = deepCopyObject(result[index]);
-			continue;
-		}
-	}
-	return result;
-}
-
-function deepCopyObject(obj) {
-	const  result = { ...obj };
+//* глубокое копирование обьект или массив
+function deepCopy(obj) {
+	const  result = isObject(obj) ? { ...obj } : [...obj];
 	for (const index of Object.keys(obj)) {
-		if (isArray(result[index])) {
-			result[index] = deepCopyArray(result[index]);
-			continue;
-		}
-		if (isObject(result[index])) {
-			result[index] = deepCopyObject(result[index]);
+		if (isArray(result[index]) || isObject(result[index])) {
+			result[index] = deepCopy(result[index]);
 			continue;
 		}
 	}
 	return result;
 }
+
--- src/helpers.js
+++ src/helpers.js
@@ -3,11 +3,6 @@ const isObject = obj => Object.prototype.toString.call(obj) === '[object Object]
 const isArray = array => Array.isArray(array);//* true если массив
 const isSamtype = (type1, type2) => Object.prototype.toString.call(type1) === Object.prototype.toString.call(type2);
 
-
-
-
-
-
 //* true если одинаковые
 module.exports = {
 	isObject,
